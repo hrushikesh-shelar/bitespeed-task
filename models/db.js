dad040ab65f65962a1d8029171465d48cf4bfbbb
hrushikeshshelar <hrushikeshshelar@urbancompany.com>
2023-08-23 13:13:56 +0530

    db configs updated

--- models/db.js
+++ models/db.js
@@ -3,10 +3,10 @@ const { Pool } = pkg;
 
 const pool = new Pool({
 	user: 'hrushikesh',
-	password: 'hrushikesh',
-	host: 'localhost',
+	password: '4MWKh0fUmvNSqo3gff8f7NaV9hJAr2Jm',
+	host: 'dpg-cjifthgcfp5c73cuqil0-a.oregon-postgres.render.com',
 	port: 5432,
-	database: 'bitespeed',
+	database: 'bitespeed_t701',
 });
 
 export { pool };
