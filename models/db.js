2d59c63b9872348cb5084b7392530b2925773851
hrushikeshshelar <hrushikeshshelar@urbancompany.com>
2023-08-23 15:35:37 +0530

    ssl true

--- models/db.js
+++ models/db.js
@@ -4,9 +4,10 @@ const { Pool } = pkg;
 const pool = new Pool({
 	user: 'hrushikesh',
 	password: '4MWKh0fUmvNSqo3gff8f7NaV9hJAr2Jm',
-	host: 'dpg-cjifthgcfp5c73cuqil0-a.oregon-postgres.render.com?ssl=true',
+	host: 'dpg-cjifthgcfp5c73cuqil0-a.oregon-postgres.render.com',
 	port: 5432,
 	database: 'bitespeed_t701',
+	ssl: true,
 });
 
 export { pool };
